948d70fc8913bb2c35bb282d513e0f938ef2eb23
k8blanco <katiewhite303@gmail.com>
2019-03-16 11:25:16 -0600

    working APIs, tasteDive and goodReads

--- models/example.js
+++ models/example.js
@@ -5,3 +5,14 @@ module.exports = function(sequelize, DataTypes) {
   });
   return Example;
 };
+
+module.exports = function(sequelize, DataTypes) {
+  var favBook = sequelize.define("favorite_books", {
+    text: DataTypes.STRING
+  });
+  return favBook;
+};
+
+//will need module exports for: favBook to write to DB
+//will need models for each: similarBooks, bookDetails, favBook
+//only favbook needs to write to db
