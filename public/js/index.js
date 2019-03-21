508f799800fdd4d8ea5006afff238fb67b0198ff
k8blanco <katiewhite303@gmail.com>
2019-03-21 11:46:33 -0600

    updated detailsPage with js

--- public/js/index.js
+++ public/js/index.js
@@ -40,20 +40,6 @@ $searchText.keypress(function (e) {
 });
 
 
-//initialize carousel -- uncomment when ready for carousel
-// $(document).ready(function(){
-//   $(".carousel.carousel-slider").carousel({
-//     fullWidth: true,
-//     indicators: true
-//   });
-// });
-    
-
-// console.log("initializing carousel");
-
-
-
-
 
 
 
